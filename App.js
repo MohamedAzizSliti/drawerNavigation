d77cfbf0a5e93493a580ccb8d10a56fe85110400
zzsliti1999 <zzsliti1999@gmail.com>
2022-03-19 13:53:13 +0100

    drawerNavigation

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './Screens/HomeScreen'
+import SettingsScreen from './Screens/SettingsScreen'
+import 'react-native-gesture-handler';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+    <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Screen name="Home" component={HomeScreen} />
+    <Drawer.Screen name="Settings" component={SettingsScreen} />
+    </Drawer.Navigator>
+    </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
